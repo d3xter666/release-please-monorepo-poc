1cfebbafdb53a9c906b246e15cc0123e785eb9e7
Your Name <you@example.com>
2025-07-16 10:06:36 +0300

    Create Jenkins like automation

--- scripts/update-dep-version.js
+++ scripts/update-dep-version.js
@@ -47,7 +47,7 @@ try {
 	execSync(`git config --global user.email "you@example.com"`);
   	execSync(`git config --global user.name "Your Name"`);
 	execSync(`git add ${pkgNameJsonPath}`);
-	execSync(`git commit -m "deps(${pkgName}): Bump version due to ${depName} update"`);
+	execSync(`git commit -m "bump(${pkgName}): Bump version due to ${depName} update"`);
 	execSync(`git push origin main`);
 	console.log('Committed the version bump.');
 } catch (err) {
